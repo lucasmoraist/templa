4db7992a7a04c3f6df275bcf6730fb7ff2ad4c50
lucasmoraist <luksmnt1101@gmail.com>
2025-08-14 20:39:57 -0300

    feat: add method to find all groups by course ID in GroupRepository

--- src/main/java/com/lucasmoraist/templa/infra/db/repository/GroupRepository.java
+++ src/main/java/com/lucasmoraist/templa/infra/db/repository/GroupRepository.java
@@ -4,9 +4,12 @@ import com.lucasmoraist.templa.infra.db.entity.GroupEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface GroupRepository extends JpaRepository<GroupEntity, UUID> {
+
+    List<GroupEntity> findAllByCourseId(UUID courseId);
 
 }
