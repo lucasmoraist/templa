0daff660c32393bf5be130524204414461db1327
lucasmoraist <luksmnt1101@gmail.com>
2025-08-14 12:26:39 -0300

    feat: initial structure

---
+++ src/main/java/com/lucasmoraist/templa/TemplaApplication.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.templa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasmoraist/templa/TemplaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmoraist.templa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemplaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
