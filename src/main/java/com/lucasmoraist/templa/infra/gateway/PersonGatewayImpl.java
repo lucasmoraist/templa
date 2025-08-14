8b756abe045d4f1adfc5d8ec7a6c5fef587c49fb
lucasmoraist <luksmnt1101@gmail.com>
2025-08-14 16:39:32 -0300

    feat: add password encoding in PersonGatewayImpl during person saving

--- src/main/java/com/lucasmoraist/templa/infra/gateway/PersonGatewayImpl.java
+++ src/main/java/com/lucasmoraist/templa/infra/gateway/PersonGatewayImpl.java
@@ -7,6 +7,7 @@ import com.lucasmoraist.templa.infra.db.repository.PersonRepository;
 import com.lucasmoraist.templa.infra.mapper.PersonMapper;
 import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Log4j2
@@ -14,9 +15,11 @@ import org.springframework.stereotype.Service;
 public class PersonGatewayImpl implements PersonGateway {
 
     private final PersonRepository repository;
+    private final PasswordEncoder passwordEncoder;
 
-    public PersonGatewayImpl(PersonRepository repository) {
+    public PersonGatewayImpl(PersonRepository repository, PasswordEncoder passwordEncoder) {
         this.repository = repository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -24,6 +27,7 @@ public class PersonGatewayImpl implements PersonGateway {
     public Person save(Person person) {
         log.debug("Saving person: {}", person);
         PersonEntity entity = PersonMapper.toEntity(person);
+        entity.getUser().setPassword(this.passwordEncoder.encode(entity.getUser().getPassword()));
         this.repository.save(entity);
 
         Person savedPerson = PersonMapper.toDomain(entity);
