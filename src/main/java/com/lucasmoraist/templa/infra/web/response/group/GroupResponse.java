587b927fd36c3a6cfcf3c55a565d4c7933d17cb5
lucasmoraist <luksmnt1101@gmail.com>
2025-08-14 19:53:11 -0300

    feat: simplify GroupResponse by removing course reference

--- src/main/java/com/lucasmoraist/templa/infra/web/response/group/GroupResponse.java
+++ src/main/java/com/lucasmoraist/templa/infra/web/response/group/GroupResponse.java
@@ -1,14 +1,11 @@
 package com.lucasmoraist.templa.infra.web.response.group;
 
-import com.lucasmoraist.templa.infra.web.response.course.CourseResponse;
-
 public record GroupResponse(
         String id,
         String dayOfWeek,
         String time,
         String duration,
-        int maxStudents,
-        CourseResponse course
+        int maxStudents
 ) {
 
 }
