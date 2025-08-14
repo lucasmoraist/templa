7b4549f12b84430a8bd19d65b4636f526c87abb9
lucasmoraist <luksmnt1101@gmail.com>
2025-08-14 16:51:20 -0300

    feat: add validation annotations to LoginRequest for email and password fields

--- src/main/java/com/lucasmoraist/templa/infra/web/request/user/LoginRequest.java
+++ src/main/java/com/lucasmoraist/templa/infra/web/request/user/LoginRequest.java
@@ -1,7 +1,13 @@
 package com.lucasmoraist.templa.infra.web.request.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record LoginRequest(
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email is invalid")
         String email,
+        @NotBlank(message = "Password is required")
         String password
 ) {
 
