afe8c22a53947aea27b603c0a1304e3b00c98ca9
lucasmoraist <luksmnt1101@gmail.com>
2025-08-15 19:50:28 -0300

    feat: enable caching in the application

--- src/main/java/com/lucasmoraist/templa/TemplaApplication.java
+++ src/main/java/com/lucasmoraist/templa/TemplaApplication.java
@@ -2,7 +2,9 @@ package com.lucasmoraist.templa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class TemplaApplication {
 
