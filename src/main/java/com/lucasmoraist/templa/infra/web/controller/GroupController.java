f5453e9878422cf21db9f48a8aa0f8970627e323
lucasmoraist <luksmnt1101@gmail.com>
2025-08-15 16:47:03 -0300

    feat: add Authorization header to getGroupById endpoint

--- src/main/java/com/lucasmoraist/templa/infra/web/controller/GroupController.java
+++ src/main/java/com/lucasmoraist/templa/infra/web/controller/GroupController.java
@@ -2,6 +2,8 @@ package com.lucasmoraist.templa.infra.web.controller;
 
 import com.lucasmoraist.templa.application.usecases.group.CreateGroupCase;
 import com.lucasmoraist.templa.application.usecases.group.GetGroupByIdCase;
+import com.lucasmoraist.templa.application.usecases.token.GetRoleByTokenCase;
+import com.lucasmoraist.templa.domain.enums.Roles;
 import com.lucasmoraist.templa.domain.model.Group;
 import com.lucasmoraist.templa.infra.mapper.GroupMapper;
 import com.lucasmoraist.templa.infra.web.request.group.GroupRequest;
@@ -19,23 +21,27 @@ public class GroupController implements GroupRoutes {
     private final CreateGroupCase createGroupCase;
     private final GetGroupByIdCase getGroupByIdCase;
 
-    public GroupController(CreateGroupCase createGroupCase, GetGroupByIdCase getGroupByIdCase) {
+    private final GetRoleByTokenCase getRoleByTokenCase;
+
+    public GroupController(CreateGroupCase createGroupCase, GetGroupByIdCase getGroupByIdCase, GetRoleByTokenCase getRoleByTokenCase) {
         this.createGroupCase = createGroupCase;
         this.getGroupByIdCase = getGroupByIdCase;
+        this.getRoleByTokenCase = getRoleByTokenCase;
     }
 
     @Override
     public ResponseEntity<?> createGroup(UUID courseId, GroupRequest request) {
         Group group = createGroupCase.execute(courseId, GroupMapper.toDomain(request));
-        Map<String, Object> response = GroupMapper.toResponse(group);
+        Map<String, Object> response = GroupMapper.toResponse(group, Roles.TEACHER);
         URI location = URI.create("/api/v1/group");
         return ResponseEntity.created(location).body(response);
     }
 
     @Override
-    public ResponseEntity<?> getGroupById(UUID id) {
+    public ResponseEntity<?> getGroupById(String authorization, UUID id) {
         Group group = getGroupByIdCase.execute(id);
-        Map<String, Object> response = GroupMapper.toResponse(group);
+        Roles role = getRoleByTokenCase.execute(authorization);
+        Map<String, Object> response = GroupMapper.toResponse(group, role);
         return ResponseEntity.ok(response);
     }
 
--- src/main/java/com/lucasmoraist/templa/infra/web/routes/GroupRoutes.java
+++ src/main/java/com/lucasmoraist/templa/infra/web/routes/GroupRoutes.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.UUID;
@@ -32,6 +33,6 @@ public interface GroupRoutes {
             security = @SecurityRequirement(name = "bearer")
     )
     @GetMapping("/{id}")
-    ResponseEntity<?> getGroupById(@PathVariable UUID id);
+    ResponseEntity<?> getGroupById(@RequestHeader("Authorization") String authorization, @PathVariable UUID id);
 
 }
