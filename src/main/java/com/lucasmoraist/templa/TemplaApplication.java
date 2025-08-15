2d081444996405a002e0d66ba9ada758f4e1552a
lucasmoraist <luksmnt1101@gmail.com>
2025-08-15 19:52:08 -0300

    feat: add Spring Cloud dependencies and enable Feign clients

--- src/main/java/com/lucasmoraist/templa/TemplaApplication.java
+++ src/main/java/com/lucasmoraist/templa/TemplaApplication.java
@@ -3,8 +3,10 @@ package com.lucasmoraist.templa;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableCaching
+@EnableFeignClients
 @SpringBootApplication
 public class TemplaApplication {
 
