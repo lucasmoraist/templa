551813a68068c48a28195c00b090e991c16046d0
lucasmoraist <luksmnt1101@gmail.com>
2025-08-15 19:50:13 -0300

    feat: refactor enrollment use case to use RegisterEnrollmentCase and integrate CacheGateway

--- src/main/java/com/lucasmoraist/templa/infra/usecases/EnrollmentUseCase.java
+++ src/main/java/com/lucasmoraist/templa/infra/usecases/EnrollmentUseCase.java
@@ -1,10 +1,10 @@
 package com.lucasmoraist.templa.infra.usecases;
 
-import com.lucasmoraist.templa.application.gateway.EnrollmentGateway;
+import com.lucasmoraist.templa.application.gateway.CacheGateway;
 import com.lucasmoraist.templa.application.gateway.GroupGateway;
 import com.lucasmoraist.templa.application.gateway.StudentGateway;
 import com.lucasmoraist.templa.application.gateway.TokenGateway;
-import com.lucasmoraist.templa.application.usecases.enrollment.EnrollCase;
+import com.lucasmoraist.templa.application.usecases.enrollment.RegisterEnrollmentCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,13 +12,13 @@ import org.springframework.context.annotation.Configuration;
 public class EnrollmentUseCase {
 
     @Bean
-    public EnrollCase enrollCase(
-            EnrollmentGateway enrollmentGateway,
+    public RegisterEnrollmentCase enrollCase(
             GroupGateway groupGateway,
             StudentGateway studentGateway,
-            TokenGateway tokenGateway
+            TokenGateway tokenGateway,
+            CacheGateway cacheGateway
     ) {
-        return new EnrollCase(enrollmentGateway, groupGateway, studentGateway, tokenGateway);
+        return new RegisterEnrollmentCase(groupGateway, studentGateway, tokenGateway, cacheGateway);
     }
 
 }
--- src/main/java/com/lucasmoraist/templa/infra/web/controller/EnrollmentController.java
+++ src/main/java/com/lucasmoraist/templa/infra/web/controller/EnrollmentController.java
@@ -1,6 +1,6 @@
 package com.lucasmoraist.templa.infra.web.controller;
 
-import com.lucasmoraist.templa.application.usecases.enrollment.EnrollCase;
+import com.lucasmoraist.templa.application.usecases.enrollment.RegisterEnrollmentCase;
 import com.lucasmoraist.templa.infra.web.routes.EnrollmentRoutes;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,15 +10,15 @@ import java.util.UUID;
 @RestController
 public class EnrollmentController implements EnrollmentRoutes {
 
-    private final EnrollCase enrollCase;
+    private final RegisterEnrollmentCase registerEnrollmentCase;
 
-    public EnrollmentController(EnrollCase enrollCase) {
-        this.enrollCase = enrollCase;
+    public EnrollmentController(RegisterEnrollmentCase registerEnrollmentCase) {
+        this.registerEnrollmentCase = registerEnrollmentCase;
     }
 
     @Override
     public ResponseEntity<Void> enroll(String authorization, UUID groupId) {
-        this.enrollCase.execute(groupId, authorization);
+        this.registerEnrollmentCase.execute(groupId, authorization);
         return ResponseEntity.noContent().build();
     }
 
